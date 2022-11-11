6ac3c6484f2efb108237b8aaf61081db867cc75c
LiTaoye <52475135+yaposhanLiTaoye@users.noreply.github.com>
2022-11-12 00:59:47 +0800

    Create BasicFunc.h

---
+++ BasicFunc.h
@@ -0,0 +1,13 @@
+#ifndef _BASICFUN_H_LTY_
+#define _BASICFUN_H_LTY_
+
+#include<stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include "log.h"
+
+bool CopyFileLTY(char *srcFile, char *dstFile);
+
+#
